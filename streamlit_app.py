c3df78b86fae75d92141ffa44504429877a58257
AK-627 <ahmedaftabkola@gmail.com>
2025-11-13 01:13:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title('🤖 MACHINE LEARNING APP')
 st.info('This app build a Machine learning model')
-with st.expander('Data')
-st.write('**Raw Data**')
-df=pd.read_csv('https://raw.githubusercontent.com/AK-627/ahmedak-ai/33af28282fed9d82d5a8b634e864f5786c67c161/tiger_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/AK-627/ahmedak-ai/33af28282fed9d82d5a8b634e864f5786c67c161/tiger_cleaned.csv')
+  df
